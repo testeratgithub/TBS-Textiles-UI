0fd10abc811b6dd4d3d958f5ea97b5ebe3a56901
arundevarajan2001 <63626641+arundevarajan2001@users.noreply.github.com>
2023-12-29 20:26:12 +0530

    Init Start-Over

---
+++ src/Login/Login.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Login.css"
+
+function LoginPage() {
+  function LoginForm() {
+    return (
+        <div class="right">
+          <div id="logo">
+            <img src="assets/img/logo1.png" alt="TBS Textiles"/>
+          </div>
+            <form action="" id="login">
+              <div class="input-field">
+                <input type="text" name="user" maxLength={16} required placeholder="User ID" />
+              </div>
+
+              <div class="input-field">
+                <input type="password"  minLength={8} maxLength={16} required placeholder="Password"/>
+                
+              </div>
+
+              <div class="forget">
+                <a href="#">Forgot password?</a>
+              </div>
+            
+              <button type="submit">Log In</button>
+              <button type="submit">Create User ID</button>
+            </form>
+        </div>
+    );
+  }
+
+  function Background() {
+    return (
+     <div className="background">
+        <LoginForm />
+      </div>
+    );
+  }
+
+  return (<Background />);
+}
+
+export default LoginPage;
